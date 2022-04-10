0ca695aa753c70cb9ae02e8084472362f1899e58
Samuel Sousa <samuelsousa064@gmail.com>
2022-04-10 20:48:51 -0300

    updating gitnore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'howmuchgas';
+export class AppComponent implements AfterViewInit {
+
+
+	private map: any;
+
+	ngAfterViewInit() {
+
+		const loader = new Loader({
+			apiKey: 'AIzaSyCn7dSI36-AtAlblWo781JKR1TZxefnBkA',
+			version: 'weekly'
+		});
+
+		loader.load().then(() => {
+			this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+				center: {lat: -34.397, lng: 150.644},
+				zoom: 8
+			});
+		})
+
+	}
+
+
+
 }
